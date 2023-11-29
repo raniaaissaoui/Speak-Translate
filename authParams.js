f129c9132a5cc31324810ff8b745450332a32847
BouzidiOumayma <126821950+BouzidiOumayma@users.noreply.github.com>
2023-11-29 12:43:48 +0100

    clone

--- authParams.js
+++ authParams.js
@@ -1,5 +1,5 @@
 export const params = {
-  api: "http://localhost/omeka-s/api/",
+  api: " http://localhost/omk_thyp_23-24_clone/api/",
   key: "hI3BJRh9ezoHfn0RpxS0aClXMmCM8kMz",
   ident: "tA9FXKrjUPW0nMt8FJBrR1i9LEoN5HX9",
   mail: "oumayma2001k@gmail.com",
